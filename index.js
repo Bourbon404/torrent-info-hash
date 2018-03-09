fe7d1c522b0cfa84d74c3dd7a18e98c7a931c801
deparsoul <deparsoul@users.noreply.github.com>
2018-03-09 11:53:28 +0800

    parseTorrent

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-module.exports = getHash;
+module.exports = parseTorrent;
 
 const bencode = require('bencode');
 const sha1 = require('simple-sha1');
 
-function getHash(buffer, filter) {
-    let torrent = bencode.decode(buffer);
-    let info = torrent.info;
-    if (filter) {
-        delete info.source;
-        delete info.private;
-    }
-    return sha1.sync(bencode.encode(info));
+function parseTorrent(buffer) {
+    let info = bencode.decode(buffer).info;
+    let torrent = {};
+    torrent.name = (info['name.utf-8'] || info.name).toString();
+    torrent.piecesHash = sha1.sync(info.pieces);
+    torrent.infoHash = sha1.sync(bencode.encode(info));
+    delete info.source;
+    delete info.private;
+    torrent.infoHashOriginal = sha1.sync(bencode.encode(info));
+    return torrent;
 }
